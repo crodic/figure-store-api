8f4f9b7d8530f3a8545d0eddbc4f311ad9b40656
Crodic <113219638+Crodic@users.noreply.github.com>
2023-07-22 13:36:39 +0700

    filter/sort

--- src/app/Controllers/product.controller.js
+++ src/app/Controllers/product.controller.js
@@ -23,10 +23,39 @@ const ProductController = {
         })
     }),
     getProducts: asyncHandler(async (req, res) => {
-        const products = await ProductModel.find();
+        const query = { ...req.query };
+        // Tách Các Field Đặc biệt ra khỏi query
+        const excludeFields = ['limit', 'sort', 'page', 'fields']
+        excludeFields.forEach(item => delete query[item]);
+
+        // 1. Filter
+        // Format lại các operators cho đúng định dạng
+        let queryString = JSON.stringify(query)
+        queryString = queryString.replace(/\b(gte|gt|lt|lte)\b/g, element => `$${element}`)
+        const formatQuery = JSON.parse(queryString);
+
+        if (query?.title) formatQuery.title = { $regex: query.title, $options: "i" }
+        let queryCommand = ProductModel.find(formatQuery);
+
+
+        // 2. Sort
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(" ");
+            queryCommand = queryCommand.sort(sortBy);
+        }
+
+        // 3.Limit
+
+
+        // 4. Pagination
+
+        const runQuery = await queryCommand.exec()
+        const counts = await ProductModel.find(formatQuery).countDocuments();
+
         return res.status(200).json({
             success: true,
-            products: products,
+            products: runQuery,
+            total: counts
         })
     }),
     deleteProduct: asyncHandler(async (req, res) => {
