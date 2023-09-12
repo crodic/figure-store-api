13bf0171c23798c8c7917eed798348da2df7d1f1
Crodic <113219638+Crodic@users.noreply.github.com>
2023-09-12 09:50:29 +0700

    update

--- index.js
+++ index.js
@@ -35,9 +35,6 @@ app.use(morgan("common"));
 Connection();
 
 // Routes
-app.use("/", (req, res) => {
-    return res.status(200).json({ msg: "OKAY" })
-})
 app.use("/v1/api/user", UserRouter);
 app.use("/v1/api/product", ProductRoute);
 app.use("/v1/api/categories", CategoriesRoute);
