0d7fcdc1049245873572961beb55ffa8bb15aaf7
Crodic <113219638+Crodic@users.noreply.github.com>
2023-09-18 15:53:41 +0700

    update

--- src/app/Config/db/DBContext.js
+++ src/app/Config/db/DBContext.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const Connection = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI_2, {
-            useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,
-            useFindAndModify: false
+            useNewUrlParser: true, useUnifiedTopology: true
         });
         console.log('Connection successfully !!!');
     } catch (error) {
